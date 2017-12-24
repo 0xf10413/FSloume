4a2dd72f3811d0c5d6a230c82e0374a4dc9c3b38
0xf10413 <florent.gattoni@ec14.ec-lyon.fr>
2017-12-24 14:15:44 +0100

    Added readme.md

---
+++ README.md
@@ -0,0 +1,16 @@
+FSloume
+=======
+
+Yet another slime volleyball clone.
+
+BUGS
+----
+* sticky ball
+* ball under the slime
+
+TODO
+----
+* Add a background
+* Add a generator for config.h/config.cpp
+* Add items
+* Add skill levels
--- main.cpp
+++ main.cpp
@@ -1,23 +1,5 @@
 #include "game/game.h"
 
-/*** BUG ***/
-/*
- * Collision par dessous : un slime entre en collision
- * de façon circulaire avec la balle par en-dessous.
- * Balle gluante : la balle peut se retrouver coincée sur un sloume
- * Config générale : passer par un générateur de cpp au lieu de variables
- * globales.
- * Config générale : problème de l'initialisation
- * Config générale : résolution sur PC trop grande
- * */
-/*** TODO ***/
-/*
- * Une IA
- * Utiliser une macro dans le resource manager
- * Des ralentisseurs/objets ?
- */
-
-
 int main ()
 {
   Config();
